22dc55f6f3178ee491ef2f24d2e761263358e7ee
Igor Rzegocki <igor@rzegocki.pl>
2019-02-19 09:33:09 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# ajgon/dot-extensions
+Just a bunch of extensions dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install ajgon/dot-extensions
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+config_home=${XDG_CONFIG_HOME:-$ELLIPSIS_HOME/.config}
+
+pkg.link() {
+    if [ ! -d "${config_home}" ]; then
+        mkdir -p "${config_home}"
+    fi
+
+    fs.link_file "${PKG_PATH}/aliases" "${config_home}/aliases"
+    fs.link_file "${PKG_PATH}/exports" "${config_home}/exports"
+    fs.link_file "${PKG_PATH}/extra" "${config_home}/extra"
+    fs.link_file "${PKG_PATH}/functions" "${config_home}/functions"
+    fs.link_file "${PKG_PATH}/aliases.$(os.platform)" "${config_home}/aliases.$(os.platform)"
+    fs.link_file "${PKG_PATH}/exports.$(os.platform)" "${config_home}/exports.$(os.platform)"
+    fs.link_file "${PKG_PATH}/extra.$(os.platform)" "${config_home}/extra.$(os.platform)"
+    fs.link_file "${PKG_PATH}/functions.$(os.platform)" "${config_home}/functions.$(os.platform)"
+
+}
+
+pkg.init() {
+    config_home=${XDG_CONFIG_HOME:-$ELLIPSIS_HOME/.config}
+
+    for file in "${config_home}"/{aliases*,exports*,extra*,functions*}; do
+        [ -r "${file}" ] && source "$file"
+    done
+    unset file
+
+    export GNUPGHOME=${XDG_CONFIG_HOME}/gnupg
+}
